e6eca167fc4a9dd0ca11adf48836082b96644b14
kat.taylor <sofia.katya.taylor@gmail.com>
2026-07-24 19:22:54 -0500

    add more basic math functions

--- script.js
+++ script.js
@@ -20,3 +20,24 @@ function divide(a,b) {
     let num = (a/b);
   return num;
 }
+
+const power = function(a,b) {
+	let total = a ** b;
+  return total;
+};
+
+const factorial = function(a) {
+	let total = 1;
+  for ( let i = a; i > 1; i--) {
+    total *= i;
+  }
+  return total;
+};
+
+const sum = function(array) {
+	let total = 0;
+  for (let i = 0; i < array.length; i++) {
+    total += array[i];
+  }
+  return total;
+};
